8d1d8607a8682e47e9fe8898341d6460cfb06e6a
Marcos653 <viniciustatuta@gmail.com>
2022-03-20 20:09:24 -0300

    deploy

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-$ns55nf(w8kte(p7qvg_od^3($ie6bh0%56t8&dj$8foy30#re
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ponto-turisticoss.herokuapp.com/']
 
 
 # Application definition
@@ -131,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = 'images'
 
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
